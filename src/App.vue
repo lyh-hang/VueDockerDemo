5b6056cc04f666a10f9ffcc8e9e021197e2e51f8
v_yyhanglai(赖雨杭) <v_yyhanglai@tencent.com>
2021-12-07 16:01:31 +0800

    测试发送请求

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png" />
     <div>
       <p>
         If Element is successfully added to this project, you'll see an
@@ -9,17 +9,39 @@
       </p>
       <el-button>jenkins-demo</el-button>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      所有用户：
+      <p v-for="user in users" :key="user._id">
+        <span>用户名：{{ user.username }} </span>
+        <span>密码：{{ user.password }} </span>
+        <span>年龄：{{ user.age }} </span>
+        <span>描述：{{ user.description }} </span>
+        <span>
+          创建时间：{{ $day(user.createDate).format('YYYY-MM-DD HH:mm:ss') }}
+        </span>
+      </p>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      users: []
+    }
+  },
   components: {
     HelloWorld
+  },
+  async mounted() {
+    const users = await axios('/api/find')
+    this.users = users
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
+import day from 'dayjs'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$day = day
 
 new Vue({
   render: h => h(App),
